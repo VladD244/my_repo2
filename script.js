ce6747656b021b1b3e2dd8c8f472c20e4c55a653
VladD244 <yo_vladdy@yahoo.com>
2024-03-04 19:02:42 +0200

    Add string data type

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let age = 29;
 let years = 5;
 console.log(age);
-console.log(years);
+// console.log(years);
 age = 36;
-
 console.log(age);
+
+let userName = 'Vlad';
+console.log('name: ');
+console.log(userName);
+
+
