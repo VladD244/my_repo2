e4f775ebf2e3035618f1a9d0dd68adf4a17c04b1
VladD244 <yo_vladdy@yahoo.com>
2024-03-04 19:14:13 +0200

    Add addition example

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ console.log(age);
 age = 36;
 console.log(age);
 
+const piNumber = 3.14;
+console.log(piNumber);
+
 let userName = 'Vlad';
 console.log('name: ');
 console.log(userName);
 
+// Operatori date numerice
 
+// adunare
+let firstNumber = 10;
+firstNumber = 12;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
